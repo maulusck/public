1d4b4981bd09f63f701ea08d896fa3ff0ad4486e
Maulusck <m4ulu5ck@localhost.localdomain>
2022-01-02 00:16:26 +0100

    update and cleanup

--- WSL-virt-manager.sh
+++ WSL-virt-manager.sh
@@ -6,11 +6,15 @@
 #
 # MAKE SURE TO HAVE YOUR XSERVER ALREADY RUNNING!!
 #
-# dependencies: qemu, ssh server, libvirt and virtlog/lock daemons, and virt-manager of course. also enable password authentication in your ssh server or copy your keys prior to this script.
-# 
-# This is just a dirty way to get your virt-manager running with [hopefully] no errors and to provide a way that 'just works'. Thank MS for not helping in all of this. Maybe you can set up a bridged connection, if you have enough ethernet ports on your PC.
+# dependencies: qemu, ssh server, libvirt and virtlog/lock daemons, and virt-manager of course.
+# also enable password authentication in your ssh server or copy your keys prior to this script.
 #
-# logs are saved in home/.vmlogs by default since libvirt and virtlog daemons cannot be started directly by systemctl/service, so we start them manually and keep them in background with logs enabled. I don't even know if it logs anything this way, but I don't have time right now.
+# This is just a dirty way to get your virt-manager running with [hopefully] no errors
+# and to provide a way that 'just works'. Thank MS for not helping in all of this. Maybe you can set up a bridged connection, if you have enough ethernet ports on your PC.
+#
+# logs are saved in home/.vmlogs by default since libvirt and virtlog daemons do not have
+# a prebuilt systemd service, so we start them manually and keep them in background with
+# logs enabled. I don't even know if it logs anything this way, I'll fix it when .
 
 if [ -d ~/.vmlogs ]; then
 echo "Daemon logs are saved in .vmlogs/ folder."
@@ -23,22 +27,23 @@ WSL_HOME=$HOME
 
 echo "Starting up..."
 echo "Ip is $CURRENT_WSL_IP"
-echo "Starting ssh server and libvirtd+virtlogd, please insert password: "
+clear && echo "Starting ssh server and libvirtd+virtlogd with sudo, please insert password: "
 sudo echo "SUDO OK"
 
 function start_services {
-service ssh restart
-libvirtd | tee -a $WSL_HOME/.vmlogs/libvirtd.log &
-virtlogd | tee -a $WSL_HOME/.vmlogs/virtlogd.log &
-}
-SV=$(declare -f start_services)
-sudo bash -c "$SV; start_services"
+	service ssh restart
+	libvirtd | tee -a $WSL_HOME/.vmlogs/libvirtd.log &
+	virtlogd | tee -a $WSL_HOME/.vmlogs/virtlogd.log &
+	}
+	SV=$(declare -f start_services)
+	sudo bash -c "$SV; start_services"
 
+clear
 printf "Services started. Warming up"
 printf "." && sleep 1 && printf "." && sleep 1 && printf "." && sleep 1 && printf "." && sleep 1 && printf "." && sleep 1 && printf ".\n"
 echo "Ready. Starting virt-manager on $CURRENT_WSL_IP..."
 
-virt-manager -c qemu+ssh://${WSL_USER}@${CURRENT_SWL_IP}/system
+virt-manager -c qemu+ssh://${WSL_USER}@${CURRENT_WSL_IP}/system | tee -a $WSL_HOME/.vmlogs/virtmanager.log &
 
-read -p "Press ENTER to clean up this page, or Ctrl+C to exit the script."
 clear
+read "Virt-manager is running. Press ENTER to close this window." 
